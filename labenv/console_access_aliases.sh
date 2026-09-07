75a246e51348d6e59db6e031acd9226bf318b1d7
Peter Csurgay <csurgay@gmail.com>
2026-09-07 09:00:40 +0200

    Add aliases for console access

---
+++ labenv/console_access_aliases.sh
@@ -0,0 +1,5 @@
+alias c0="sudo podman exec -it -u devops -w /home/devops/ansible/lessons ansible bash" 
+alias c1="sudo podman exec -it -u devops -w /home/devops host1 bash" 
+alias c2="sudo podman exec -it -u devops -w /home/devops host2 bash" 
+alias c3="sudo podman exec -it -u devops -w /home/devops host3 bash" 
+
