0bf0a3762eac0ed176ac4161eb53324cfc3d6840
Peter Csurgay <csurgay@gmail.com>
2026-09-07 08:49:35 +0200

    Add aliases for console access

--- labenv/run.sh
+++ labenv/run.sh
@@ -14,6 +14,13 @@ log() {
         fi
 }
 
+log "Setting up aliases for console access for hosts"
+
+alias c0="sudo podman exec -it -u devops -w /home/devops/ansible/lessons ansible bash"
+alias c1="sudo podman exec -it -u devops -w /home/devops host1 bash"
+alias c2="sudo podman exec -it -u devops -w /home/devops host2 bash"
+alias c3="sudo podman exec -it -u devops -w /home/devops host3 bash"
+
 log "Usage script starting..."
 
 sudo ./ansible_node/run_containers.sh
